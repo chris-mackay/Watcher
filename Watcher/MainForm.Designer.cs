011b18495a732e852a0537bf758583f73d4e6f46
Christopher Mackay <cmackay732@gmail.com>
2023-10-29 17:40:09 -0400

    commit

--- Watcher/MainForm.Designer.cs
+++ Watcher/MainForm.Designer.cs
@@ -28,21 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.directoriesListbox = new System.Windows.Forms.ListBox();
+            this.logListbox = new System.Windows.Forms.ListBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
+            // 
+            // directoriesListbox
+            // 
+            this.directoriesListbox.FormattingEnabled = true;
+            this.directoriesListbox.Location = new System.Drawing.Point(12, 12);
+            this.directoriesListbox.Name = "directoriesListbox";
+            this.directoriesListbox.Size = new System.Drawing.Size(538, 121);
+            this.directoriesListbox.TabIndex = 0;
+            // 
+            // logListbox
+            // 
+            this.logListbox.FormattingEnabled = true;
+            this.logListbox.Location = new System.Drawing.Point(12, 152);
+            this.logListbox.Name = "logListbox";
+            this.logListbox.Size = new System.Drawing.Size(538, 290);
+            this.logListbox.TabIndex = 1;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Watcher";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 452);
+            this.Controls.Add(this.logListbox);
+            this.Controls.Add(this.directoriesListbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Watcher";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox directoriesListbox;
+        private System.Windows.Forms.ListBox logListbox;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
 
--- Watcher/MainForm.cs
+++ Watcher/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,47 @@ namespace Watcher
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var watcher = new FileSystemWatcher(@"C:\Users\Cmack\Desktop");
+            watcher.Created += OnCreated;
+
+            watcher.NotifyFilter = NotifyFilters.FileName;
+            watcher.Filter = "";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            watcher.SynchronizingObject = this;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            notifyIcon.Icon = Properties.Resources.Watcher;
+            notifyIcon.Text = "Watcher";
+            notifyIcon.Visible = true;
+            notifyIcon.BalloonTipTitle = "File added";
+            notifyIcon.BalloonTipText = e.FullPath;
+            notifyIcon.ShowBalloonTip(1000);
+
+            logListbox.Items.Add($"File added : {e.FullPath}");
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
         }
     }
 }
